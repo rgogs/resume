ff45b8cc6ea3135cb2100e904f251f23517e51b5
Ralph Goguanco <ralphgoguanco@gmail.com>
2024-07-03 16:47:29 -0700

    Updates

---
+++ precache-manifest.72455c3fd2e02c2dee81c5a4e6af5e10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82da05ed1582eda5723a4d7612e5615",
+    "url": "/github.com/rgogs/index.html"
+  },
+  {
+    "revision": "91e892060de36c1c2d77",
+    "url": "/github.com/rgogs/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "768f268141eee2e0b05d",
+    "url": "/github.com/rgogs/static/css/main.9675614a.chunk.css"
+  },
+  {
+    "revision": "91e892060de36c1c2d77",
+    "url": "/github.com/rgogs/static/js/2.e5cf5d09.chunk.js"
+  },
+  {
+    "revision": "269d9e3cd9a95cddb445a7d27d4b3186",
+    "url": "/github.com/rgogs/static/js/2.e5cf5d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768f268141eee2e0b05d",
+    "url": "/github.com/rgogs/static/js/main.ed02123b.chunk.js"
+  },
+  {
+    "revision": "0c1b9947c332f6d98353",
+    "url": "/github.com/rgogs/static/js/runtime-main.b61e90f3.js"
+  },
+  {
+    "revision": "08a512091aa680ce944fea306337fd26",
+    "url": "/github.com/rgogs/static/media/resume.08a51209.pdf"
+  }
+]);
